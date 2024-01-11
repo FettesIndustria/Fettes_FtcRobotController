5b748ec7acce20d5820930462bcf4c81aa12b7dc
Luca Armstrong <79023720+LucaArmstrong@users.noreply.github.com>
2024-01-11 15:16:36 +0000

    Added orthogonal directional movement to the dc motors, and modified the motor test class to test the servo motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main_Luca.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main_Luca.java
@@ -1,54 +1,9 @@
-/*import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-
-import org.firstinspires.ftc.teamcode.ControllerInputHandler;
-import org.firstinspires.ftc.teamcode.MotorRun;
-
-@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "Main", group = "TeleOp")
-public class Main extends OpMode {
-
-    private ControllerInputHandler controllerInput;
-    private DcMotor coreHexMotor;
-    private MotorRun coreHexMotorClass;
-    private DcMotor hdHexMotor;
-    private MotorRun hdHexMotorClass;
-
-
-    @Override
-    public void init() {
-        /*controllerInput = new ControllerInputHandler(gamepad1);
-        coreHexMotorClass = new MotorRun(coreHexMotor, 0, "forward"); // power, direction
-        coreHexMotor = hardwareMap.get(DcMotor.class, "coreHexMotor");
-        coreHexMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-
-        hdHexMotorClass = new MotorRun(hdHexMotor, 0, "forward");
-        hdHexMotor = hardwareMap.get(DcMotor.class, "left");
-        hdHexMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        hdHexMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-    }
-    @Override
-    public void loop() {
-        boolean isButtonAPressed = controllerInput.isButtonPressed('a');
-        boolean isButtonBPressed = controllerInput.isButtonPressed('b');
-        boolean isButtonXPressed = controllerInput.isButtonPressed('x');
-        boolean isButtonYPressed = controllerInput.isButtonPressed('y');
-
-        //coreHexMotor.setPower(isButtonAPressed ? 0.5 : 0);
-        hdHexMotor.setPower(isButtonBPressed ? 0.5 : 0);
-
-        telemetry.addData("Button A Pressed ", false); // a pressed
-        telemetry.addData("Button B Pressed ", false); // b pressed
-        telemetry.update();
-    }
-}
-*/
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import java.lang.Math;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "Main_Luca", group = "TeleOp")
 public class Main_Luca extends OpMode {
@@ -57,8 +12,7 @@ public class Main_Luca extends OpMode {
     private DcMotor rightMotor;
     private DcMotor hdHexMotor;
 
-    public double speed = 0.5;
-    public double turn_speed = 0.1;
+    public double turn_speed = 0.1; // constant turn speed, later maybe control using triggers as opposed to bumpers
 
     @Override
     public void init() {
@@ -78,20 +32,25 @@ public class Main_Luca extends OpMode {
     }
     @Override
     public void loop() {
-        boolean isButtonAPressed = controllerInput.isButtonPressed('a');
-        boolean isButtonBPressed = controllerInput.isButtonPressed('b');
-        boolean isButtonXPressed = controllerInput.isButtonPressed('x');
-        boolean isButtonYPressed = controllerInput.isButtonPressed('y');
+        double leftStickX = controllerInput.getLeftStickX();
+        double leftStickY = -controllerInput.getLeftStickY();    // also negate the sign
+        telemetry.addData("Left stick has X value:\t", leftStickX);
+        telemetry.addData("Left stick has Y value:\t", leftStickY);
+
+        double sqrt2 = Math.sqrt(2);
+        double s = 0.5;  // sensitivity constant
+
+        // use speed equations for AD and BC motors
+        double v_ad = (s / sqrt2) * (leftStickY + leftStickX);
+        double v_bc = (s / sqrt2) * (leftStickY - leftStickX);
+
+        // set motor speeds
+        // motors not defined yet
 
         boolean leftBumper = controllerInput.leftBumper();
         boolean rightBumper = controllerInput.rightBumper();
 
-        float leftStickX = controllerInput.getLeftStickX();
-        float leftStickY = controllerInput.getLeftStickY();
-        telemetry.addData("Left stick has X value:\t", leftStickX);
-        telemetry.addData("Left stick has Y value:\t", leftStickY);
-
-        // adjust speed
+        // set turn speeds (turning has higher priority than movement)
         if (leftBumper) {
             leftMotor.setPower(-turn_speed);
             rightMotor.setPower(turn_speed);
@@ -102,17 +61,9 @@ public class Main_Luca extends OpMode {
             rightMotor.setPower(-turn_speed);
         }
 
-        // adjust direction
-        speed = leftStickY;
-
-        //rightMotor.setPower(isButtonXPressed ? speed : 0);
-        //leftMotor.setPower(isButtonXPressed ? speed : 0);
-        rightMotor.setPower(speed);
-        leftMotor.setPower(speed);
-
-        telemetry.addData("Button A Pressed ", isButtonAPressed); // a pressed
-        telemetry.addData("Button B Pressed ", isButtonBPressed); // b pressed
-        telemetry.addData("The speed is:\t", speed);
+        // write to/update telemetry
+        telemetry.addData("AD motor speeds:\t", v_ad);
+        telemetry.addData("BC motor speeds:\t", v_bc);
         telemetry.update();
     }
 }
